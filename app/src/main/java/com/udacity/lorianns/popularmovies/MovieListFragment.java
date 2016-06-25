95151770fd7f23b2da918a796caae59edbf356ca
Lorianns <lorianns615@gmail.com>
2016-06-25 19:24:17 -0400

    - added setting in the strings xml. - added menu setting action. - created a Movie List Fragment and initialize gridview control.

---
+++ app/src/main/java/com/udacity/lorianns/popularmovies/MovieListFragment.java
@@ -0,0 +1,41 @@
+package com.udacity.lorianns.popularmovies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieListFragment extends Fragment {
+
+
+    public MovieListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_movie_list, container, false);
+
+        initControls(rootView);
+
+        return rootView;
+    }
+
+
+    private void initControls(View rootView) {
+
+        GridView gridview = (GridView) rootView.findViewById(R.id.gridView);
+
+    }
+
+}
